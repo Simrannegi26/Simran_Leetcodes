20b5f1e5f951d32db8423e8e59d1f1e4d020641e
Simran Negi <157865950+Simrannegi26@users.noreply.github.com>
2026-07-19 00:50:22 +0530

    Time: 0 ms (100%), Space: 43.3 MB (81.31%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int dp[][] = new int [m][n];
+
+        // If starting cell is blocked
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Obstacle
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                      // First row
+                else if (i == 0 && j > 0)
+                    dp[i][j] = dp[i][j - 1];
+
+                // First column
+                else if (j == 0 && i > 0)
+                    dp[i][j] = dp[i - 1][j];
+
+                // Remaining cells
+                else if (i > 0 && j > 0)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];  
+    }
+}
