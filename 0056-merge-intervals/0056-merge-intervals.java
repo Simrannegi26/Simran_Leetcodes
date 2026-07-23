8e3df1fd240fec7f1de71241991def11ed1c1b7a
Simran Negi <157865950+Simrannegi26@users.noreply.github.com>
2026-07-23 19:18:37 +0530

    Time: 12 ms (9.79%), Space: 49.4 MB (6.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -7,20 +7,21 @@ class Solution {
         Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
 
         List<int[]> result = new ArrayList<>();  //make a list of arrays- named result
-        int[] newInterval = intervals[0];        // strt with 0 indexed interval
+        int[] newInterval = intervals[0];        // strt with 0 indexed interval , ex - [1,3]= newInterval
         for(int[] i : intervals ){
 
-            if(i[0] <= newInterval[1]){                // overlapping intervals [1,3],[2,6]
-                newInterval[1]= Math.max(newInterval[1], i[1]);    // keep larger ending point
+            if(i[0] <= newInterval[1]){                // overlapping intervals [2,6],[1,3], 2<=3 they overlap
+                newInterval[1]= Math.max(newInterval[1], i[1]);    // keep larger ending point, newInterval=[1,6]
             }
             else{
-                result.add(newInterval);
-                newInterval= i;
+                result.add(newInterval);            // no overlap , ex- [1,6],[8,10]
+                newInterval= i;                     // newInterval= [8,10]
                
             }
         }
         result.add(newInterval);
-        return result.toArray(new int[result.size()][]);
+        // Convert ArrayList<int[]> into int[][].
+        return result.toArray(new int[result.size()][]);     
 
     }
 }
