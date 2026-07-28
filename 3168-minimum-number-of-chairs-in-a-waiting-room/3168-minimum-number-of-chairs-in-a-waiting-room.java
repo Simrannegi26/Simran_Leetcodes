61cd0b84bfe67a7feaf4f9e875146089420ea0ad
Simran Negi <157865950+Simrannegi26@users.noreply.github.com>
2026-07-28 23:20:58 +0530

    Time: 1 ms (100%), Space: 43.7 MB (29.36%) - LeetHub

---
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumChairs(String s) {
+        int curr= 0;
+        int max= 0;
+
+        for(char ch: s.toCharArray()){
+            if(ch== 'E'){
+                curr++;
+                max= Math.max(max, curr);
+            }
+            else{
+                curr--;
+            }
+
+        }
+        return max;
+    }
+}
